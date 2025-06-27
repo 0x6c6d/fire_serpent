544152665dec2c730292b6eae0ed9227125b5ee0
Lucas Menke <lucas_menke@icloud.com>
2025-06-27 15:07:36 +0200

    > logs

--- um/src/hacks/hacks.cpp
+++ um/src/hacks/hacks.cpp
@@ -7,7 +7,9 @@
 
 void hacks::visuals_thread() noexcept
 {
-	static bool has_logged = false;
+	static bool log_msg = true;
+	static bool log_glow = true;
+	static bool log_radar = true;
 	
 	while (gui::is_runninig) 
 	{
@@ -16,7 +18,7 @@ void hacks::visuals_thread() noexcept
 		const auto local_player = driver::rpm<std::uintptr_t>(globals::client + offsets::dwLocalPlayerPawn);
 		if (!local_player)
 		{
-			if (!has_logged)
+			if (log_msg)
 				logs::log(logs::Level::Error, "[e] unable to find local_player: %p\n", globals::client + offsets::dwLocalPlayerPawn);
 			continue;
 		}
@@ -25,7 +27,7 @@ void hacks::visuals_thread() noexcept
 		const auto entity_list = driver::rpm<std::uintptr_t>(globals::client + offsets::dwEntityList);
 		if (!entity_list)
 		{
-			if (!has_logged)
+			if (log_msg)
 				logs::log(logs::Level::Error, "[e] unable to find entity_list: %p\n", globals::client + offsets::dwEntityList);
 			continue;
 		}
@@ -35,7 +37,7 @@ void hacks::visuals_thread() noexcept
 			uintptr_t list_entry1 = driver::rpm<std::uintptr_t>(entity_list + (8 * ((i & 0x7FFF) >> 9)) + 0x10);
 			if (!list_entry1)
 			{
-				if (!has_logged)
+				if (log_msg)
 					logs::log(logs::Level::Warning, "[w] unable to find list_entry1 for player %d: %p\n", i, entity_list + (8 * ((i & 0x7FFF) >> 9)) + 0x10);
 				continue;
 			}
@@ -43,7 +45,7 @@ void hacks::visuals_thread() noexcept
 			uintptr_t player_controller = driver::rpm<std::uintptr_t>(list_entry1 + 120 * (i & 0x1FF));
 			if (!player_controller)
 			{
-				if (!has_logged)
+				if (log_msg)
 					logs::log(logs::Level::Warning, "[w] unable to find player_controller for player %d: %p\n", i, list_entry1 + 120 * (i & 0x1FF));
 				continue;
 			}
@@ -51,7 +53,7 @@ void hacks::visuals_thread() noexcept
 			uintptr_t player_pawn = driver::rpm<std::uintptr_t>(player_controller + offsets::m_hPlayerPawn);
 			if (!player_pawn)
 			{
-				if (!has_logged)
+				if (log_msg)
 					logs::log(logs::Level::Warning, "[w] unable to find player_pawn for player %d: %p\n", i, player_controller + offsets::m_hPlayerPawn);
 				continue;
 			}
@@ -59,7 +61,7 @@ void hacks::visuals_thread() noexcept
 			uintptr_t list_entry2 = driver::rpm<std::uintptr_t>(entity_list + 0x8 * ((player_pawn & 0x7FFF) >> 9) + 16);
 			if (!list_entry2)
 			{
-				if (!has_logged)
+				if (log_msg)
 					logs::log(logs::Level::Warning, "[w] unable to find list_entry2 for player %d: %p\n", i, ((player_pawn & 0x7FFF) >> 9) + 16);
 				continue;
 			}
@@ -67,7 +69,7 @@ void hacks::visuals_thread() noexcept
 			uintptr_t p_cs_player_pawn_ptr = driver::rpm<std::uintptr_t>(list_entry2 + 120 * (player_pawn & 0x1FF));
 			if (!p_cs_player_pawn_ptr)
 			{
-				if (!has_logged)
+				if (log_msg)
 					logs::log(logs::Level::Warning, "[w] unable to find p_cs_player_pawn_ptr for player %d: %p\n", i, list_entry2 + 120 * (player_pawn & 0x1FF));
 				continue;
 			}
@@ -80,14 +82,18 @@ void hacks::visuals_thread() noexcept
 			if (health <= 0)
 				continue;
 
-			if (!has_logged)
+			if (log_msg)
 				logs::log(logs::Level::Info, "[i] found everything for player %d", i);
 
 			// glow hack
 			if (globals::glow)
 			{
-				if (!has_logged)
-					logs::log(logs::Level::Info, "[i] glow hack active: %p", p_cs_player_pawn_ptr);
+				if (log_glow)
+				{
+					logs::log(logs::Level::Info, "[i] glow hack active");
+					log_glow = false;
+				}
+					
 				DWORD colorArgb = ((DWORD)(globals::glow_color[3]) << 24) |
 					((DWORD)(globals::glow_color[0]) << 16) |
 					((DWORD)(globals::glow_color[1]) << 8) |
@@ -96,16 +102,31 @@ void hacks::visuals_thread() noexcept
 				//driver::wpm((p_cs_player_pawn_ptr + offsets::m_Glow + offsets::m_glowColorOverride), colorArgb);
 				//driver::wpm((p_cs_player_pawn_ptr + offsets::m_Glow + offsets::m_bGlowing), 1);
 			}
+			else
+			{
+				if(!log_glow)
+					logs::log(logs::Level::Info, "[i] glow hack inactive");
+				log_glow = true;
+			}
 
 			// radars hack
 			if (globals::radar)
 			{
-				if (!has_logged)
-					logs::log(logs::Level::Info, "[i] radar hack active: %p", player_pawn + offsets::m_bSpotted);
+				if (log_radar)
+				{
+					logs::log(logs::Level::Info, "[i] radar hack active");
+					log_radar = false;
+				}
 				//driver::wpm(player_pawn + offsets::m_bSpotted, true);
+			}
+			else
+			{
+				if (!log_radar)
+					logs::log(logs::Level::Info, "[i] radar hack inactive");
+				log_radar = true;
 			}
 		}
 
-		has_logged = true;
+		log_msg = false;
 	}
 }
